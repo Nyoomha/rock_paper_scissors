beae324f268906cffaecee7b5e82f156a8146a82
Nyoomha <112516013+Nyoomha@users.noreply.github.com>
2024-07-21 23:03:55 -0700

    main changes

--- RPS.py
+++ RPS.py
@@ -1,37 +1,35 @@
 import random
+from tkinter import *
 
-options = ['rock', 'paper', 'scissors']
-
-print("\nHello! Welcome to Rock, Paper, and Scissors\n")
-
-choice = input("Enter your option: ").lower()
-computer_choice = random.choice(options)
-
-#rock_computer():
-
-if choice not in ['rock', 'paper', 'scissors']:
-    print("Please try again")
-else:
-    if choice == 'rock':
-        if computer_choice == 'paper':
-            print(computer_choice,"beats", choice, "I won!")
-        elif computer_choice == 'scissors':
-            print(choice,"beats", computer_choice, "you won!")
+def get_player_choice():
+    while True:
+        choice = input("Enter your option: ").lower()
+        if choice in ['rock', 'paper', 'scissors']:
+            return choice
         else:
-            print("We both selected", choice, "its a tie!")
-        
-    if choice == 'paper':
-        if computer_choice == 'scissors':
-            print(computer_choice,"beats", choice, "I won!")
-        elif computer_choice == 'rock':
-            print(choice,"beats", computer_choice, "you won!")
-        else:
-            print("We both selected", choice, "its a tie!")
+            print("Please try again\n")
 
-    if choice == 'scissors':
-        if computer_choice == 'rock':
-            print(computer_choice,"beats", choice, "I won!")
-        elif computer_choice == 'paper':
-            print(choice,"beats", computer_choice, "you won!")
-        else:
-            print("We both selected", choice, "its a tie!")
+def computer_choice():
+    options = ['rock', 'paper', 'scissors']
+    return random.choice(options)
+
+
+def det_winner(player_choice_result, computer_choice_result):
+    if (player_choice_result == 'rock' and computer_choice_result == 'scissors') or \
+        (player_choice_result == 'scissors' and computer_choice_result == 'paper') or \
+        (player_choice_result == 'paper' and computer_choice_result == 'rock'):
+        print (player_choice_result, "beats", computer_choice_result, "you win!" )
+    elif player_choice_result == computer_choice_result:
+        print("we both chose", player_choice_result, "its a tie!")
+    else:
+        print(computer_choice_result, "beats", player_choice_result, "I win!")
+
+
+def main():
+    print("\nHello! Welcome to Rock, Paper, and Scissors\n")
+    computer_choice_result = computer_choice()
+    player_choice_result = get_player_choice()
+    det_winner(player_choice_result, computer_choice_result)
+
+if __name__ == "__main__":
+    main()
---
+++ gui_rps.py
@@ -0,0 +1,53 @@
+import random
+from tkinter import *
+import tkinter as ttk
+
+root = ttk.Tk()
+root.geometry("600x400")
+
+choice_var = ttk.StringVar()
+
+def submit():
+ 
+    choice = choice_var.get()
+     
+    print("The name is : " + choice)
+     
+    choice_var.set("")
+
+
+options = ['rock', 'paper', 'scissors']
+
+print("\nHello! Welcome to Rock, Paper, and Scissors\n")
+
+choice = input("Enter your option: ").lower()
+computer_choice = random.choice(options)
+
+#rock_computer():
+
+if choice not in ['rock', 'paper', 'scissors']:
+    print("Please try again")
+else:
+    if choice == 'rock':
+        if computer_choice == 'paper':
+            print(computer_choice,"beats", choice, "I won!")
+        elif computer_choice == 'scissors':
+            print(choice,"beats", computer_choice, "you won!")
+        else:
+            print("We both selected", choice, "its a tie!")
+        
+    if choice == 'paper':
+        if computer_choice == 'scissors':
+            print(computer_choice,"beats", choice, "I won!")
+        elif computer_choice == 'rock':
+            print(choice,"beats", computer_choice, "you won!")
+        else:
+            print("We both selected", choice, "its a tie!")
+
+    if choice == 'scissors':
+        if computer_choice == 'rock':
+            print(computer_choice,"beats", choice, "I won!")
+        elif computer_choice == 'paper':
+            print(choice,"beats", computer_choice, "you won!")
+        else:
+            print("We both selected", choice, "its a tie!")
